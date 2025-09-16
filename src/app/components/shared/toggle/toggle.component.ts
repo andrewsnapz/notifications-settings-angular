aec988adf05d0e79ae17f514994b97220eed892f
Andrew Shin <andyrpg@gmail.com>
2025-09-16 15:40:02 -0700

    completed component, need to write test

---
+++ src/app/components/shared/toggle/toggle.component.ts
@@ -0,0 +1,64 @@
+import { Component, computed, input, output, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-toggle',
+  imports: [],
+  templateUrl: './toggle.component.html',
+  styleUrl: './toggle.component.scss',
+})
+export class ToggleComponent {
+  id = input.required<string>();
+  size = input<'sm' | 'md'>('sm');
+  labelActiveText = input.required<string>();
+  labelDeactiveText = input.required<string>();
+  isLabelHidden = input.required<boolean>();
+  defaultToggled = input<boolean>(false);
+  isDisabled = input.required<boolean>();
+
+  onActiveToggle = output();
+  onDeactiveToggle = output();
+
+  isToggled = signal(this.defaultToggled());
+
+  toggleClasses = computed(() => ({
+    toggleContainer: {
+      ['toggle-container']: true,
+    },
+    toggleControl: {
+      toggle: true,
+      ['toggle-deactive']: !this.isToggled(),
+      ['toggle-active']: this.isToggled(),
+      [`toggle-${this.size()}`]: true,
+    },
+    toggleBall: {
+      ['toggle-ball']: true,
+      ['toggle-ball-disabled']: this.isDisabled(),
+      ['toggle-ball-deactive']: !this.isToggled(),
+      ['toggle-ball-active']: this.isToggled(),
+      [`toggle-ball-${this.size()}`]: true,
+    },
+    label: {
+      ['visually-hidden']: this.isLabelHidden(),
+    },
+  }));
+
+  onToggleChange() {
+    console.log(this.isToggled());
+    if (this.isToggled()) {
+      this.onDeactiveToggle.emit();
+    } else {
+      this.onActiveToggle.emit();
+    }
+    this.isToggled.update((prev) => !prev);
+
+    console.log(this.isToggled());
+  }
+}
+
+/*
+  toggle functionalities
+    - isToggled: input
+    - isDisabled: input
+    - what to do if the the toggle is toggled (func) -> must change isToggled
+    - what to do if the toggle is un-toggled (func) -> must change isToggled
+*/
