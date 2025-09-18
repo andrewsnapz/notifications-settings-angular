8aac4c257f6b5bad286886bb2fa2c91b2fb64a92
Andrew Shin <andyrpg@gmail.com>
2025-09-18 13:33:52 -0700

    attempted to add grid components that are not working

--- src/app/components/layout/grid/grid-item/grid-item.component.ts
+++ src/app/components/layout/grid/grid-item/grid-item.component.ts
@@ -1,14 +1,17 @@
-import { Component, input } from '@angular/core';
-import { NgClass } from '@angular/common';
+import { Component, HostBinding, input } from '@angular/core';
 
 @Component({
   selector: 'app-grid-item',
-  imports: [NgClass],
-  templateUrl: './grid-item.component.html',
+  template: `<ng-content />`,
   styleUrl: './grid-item.component.scss',
 })
 export class GridItemComponent {
-  colMobile = input<number>(4);
-  colTablet = input<number>(6);
-  colDesktop = input<number>(12);
+  colMobile = input(4);
+  colTablet = input(6);
+  colDesktop = input(12);
+
+  @HostBinding('class')
+  get hostClasses(): string {
+    return `col-mobile-${this.colMobile()} col-tablet-${this.colTablet()} col-desktop-${this.colDesktop()}`;
+  }
 }
