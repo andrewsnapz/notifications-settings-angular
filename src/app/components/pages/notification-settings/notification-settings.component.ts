bdbfc4d0fe8575bfc372bdd390de39f53fb524f5
Andrew Shin <andyrpg@gmail.com>
2025-09-19 12:13:32 -0700

    removed css variables in place for scss mixins/variables

--- src/app/components/pages/notification-settings/notification-settings.component.ts
+++ src/app/components/pages/notification-settings/notification-settings.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 import { BackendErrorComponent } from '../../shared/error/backend-error/backend-error.component';
+import { ButtonComponent } from '../../shared/button/button.component';
 
 @Component({
   selector: 'app-notification-settings',
-  imports: [BackendErrorComponent],
+  imports: [BackendErrorComponent, ButtonComponent],
   templateUrl: './notification-settings.component.html',
   styleUrl: './notification-settings.component.scss',
 })
